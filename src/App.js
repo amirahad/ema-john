e8207503613f56e2a0800998ff6dd109a872f73a
amirahad <arahad4783@gmail.com>
2022-10-13 17:36:15 +0600

    route setup adde

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
 
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Header from './components/header/Header';
+import Inventory from './components/inventory/Inventory';
+import NotFound from './components/NotFound/NotFound';
+import Review from './components/review/Review';
 import Shop from './components/shop/Shop';
 
 
 function App() {
   return (
-    <div >
-      <Header />
-      <Shop />
+    <div>
+
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route exact path="/" >
+            <Shop />
+          </Route>
+          <Route path="/shop" >
+            <Shop />
+          </Route>
+          <Route exact path="/review" >
+            <Review />
+          </Route>
+          <Route path="/inventory" >
+            <Inventory />
+          </Route>
+          <Route path='*' >
+            <NotFound />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
